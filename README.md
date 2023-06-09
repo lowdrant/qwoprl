a8a325c59ce2d004e045b048ec4fce93ebba8349
lowdrant <lmanderson42@gmail.com>
2023-06-09 12:09:54 -0800

    virtual env/pip3 install scripts+docs

--- README.md
+++ README.md
@@ -3,5 +3,12 @@ Apply reinforcement learning/other methods to QWOP
 
 # Dependencies
 * Python3
-* matplotlib
-* numpy
+* pip3
+
+# Quickstart
+## Virtual Environment
+If you can run bash,
+```
+$ ./bin/init-setup.sh
+```
+will create a virtual environment and install everything. Otherwise, you can directly use the pip3 requirements file: [bin/requirements.txt](bin/requirements.txt).
---
+++ bin/init-setup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#
+# Setup repo
+#
+reporoot="$(dirname "$(dirname "$(realpath "$0")")")"
+
+# User-Configurable Constants
+REQ_PROGRAMS=( pip3 )  # array of commands
+INSTALLER='apt install'  # installer command WITHOUT sudo
+ENVDIR="$reporoot"/env  # envdir
+REQTXT="$reporoot"/bin/requirements.txt  # pip3 requirements file
+
+# Install Check
+for p in "${REQ_PROGRAMS[@]}"; do
+    if ! which $p &> /dev/null; then
+        echo "Could not find: $p. Attempting install..."
+        sudo $INSTALLER $p
+    else
+        echo "$p already installed! Skipping"
+    fi
+done
+
+# Setup VEnv
+echo "Configuring virtualenv at $ENVDIR"
+python3 -m venv "$ENVDIR"
+pushd "$reporoot" &> /dev/null
+    source "$ENVDIR"/bin/activate
+    pip3 install -r "$REQTXT"
+popd &> /dev/null
