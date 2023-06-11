c16307dfe294c55cfea9bfc2372b4d3937abcf2a
lowdrant <lmanderson42@gmail.com>
2023-06-10 17:08:24 -0700

    implement save/load in lander

--- lander.py
+++ lander.py
@@ -6,6 +6,7 @@ from argparse import ArgumentParser
 
 import matplotlib.pyplot as plt
 from numpy import linspace, pi, vstack
+from numpy.random import seed
 
 import gymnasium as gym
 from myrl import DiscretizerFactory, QTable, plotrl
@@ -20,14 +21,17 @@ parser.add_argument('--alpha', default=0.5, type=float,
 parser.add_argument('--gamma', default=0.5, type=float,
                     help='Reward parameter (def:0.5)')
 parser.add_argument('--render-mode', default=None, help='gym render mode')
+parser.add_argument('--fn', help='filename for load/save',
+                    default='lander.txt')
+parser.add_argument('--load', action='store_true', help='load from fn')
+parser.add_argument('--save', action='store_true', help='load to fn')
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    # Gym Setup
-    env = gym.make("LunarLander-v2", render_mode=args.render_mode)
 
-    # Policy Setup
+    # Setup
+    env = gym.make("LunarLander-v2", render_mode=args.render_mode)
     ds = DiscretizerFactory([
         {-0.1: '<', 0: '<', 0.1: '>'},  # x
         {0: '<', 0.1: '<', 1: '<'},  # y
@@ -36,14 +40,18 @@ if __name__ == '__main__':
         {v: '<' for v in linspace(0.1, pi, 4)},  # theta
         {-1: '<', -0.1: '<', 1: '>', 0.1: '>'}  # omega
     ])
-    policy = QTable(ds.n, 4, env.action_space.sample, ds,
-                    eps=args.eps, alpha=args.alpha, gamma=args.gamma)
+    if args.load:
+        policy = QTable(ds.n, 4, env.action_space.sample, ds)
+        policy.load(args.fn)
+    else:
+        policy = QTable(ds.n, 4, env.action_space.sample, ds,
+                        eps=args.eps, alpha=args.alpha, gamma=args.gamma)
 
     # Experiment
-    olog, rlog, alog, ilog = [], [], [], []
-    N = args.N
+    olog, rlog, alog = [], [], []
     state, info = env.reset(seed=42)
-    for _ in range(N):
+    seed(0)
+    for _ in range(args.N):
         # Action
         action = policy.pick_action(state[:-2])
         next_state, reward, terminated, truncated, info = env.step(action)
@@ -59,6 +67,9 @@ if __name__ == '__main__':
         rlog.append(reward)
     env.close()
 
+    # End
+    if args.save:
+        policy.save(args.fn)
     olog = vstack(olog).T
     plotrl(olog, rlog, alog)
     plt.show()
