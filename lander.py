94a7b9d7d05c79b26f0d4a007e525a5daa8da74e
lowdrant <lmanderson42@gmail.com>
2023-06-11 11:30:00 -0400

    lander code cleanup

--- lander.py
+++ lander.py
@@ -11,33 +11,40 @@ from numpy.random import seed
 import gymnasium as gym
 from myrl import DiscretizerFactory, QTable, plotrl
 
-parser = ArgumentParser('Lunar lander RL playground')
-parser.add_argument(
-    'N', nargs='?', help='number of experiments to run', default=10, type=int)
-parser.add_argument('--eps', default=0.25, type=float,
-                    help='random action probability (def:0.25)')
-parser.add_argument('--alpha', default=0.5, type=float,
-                    help='learning rate (def:0.5)')
-parser.add_argument('--gamma', default=0.5, type=float,
-                    help='discount parameter (def:0.5)')
-parser.add_argument('--render-mode', default=None, help='gym render mode')
-parser.add_argument('--load-fn', help='filename for load. Default: lander.txt',
-                    default='lander.txt')
-parser.add_argument('--save-fn', help='filename for save. Default: lander.txt',
-                    default='lander.txt')
-parser.add_argument('--load', action='store_true', help='load from fn flag')
-parser.add_argument('--save', action='store_true', help='save to fn flag')
-parser.add_argument('--overwrite', action='store_true',
-                    help='overwrite save file flag')
-parser.add_argument('--load-params', action='store_true',
-                    help='load params from fn flag')
-parser.add_argument('--plot', action='store_true', help='plot data flag')
-parser.add_argument('--epoch-size', default=10000, type=int,
-                    help='number of experiments per epoch')
-parser.add_argument('--save-epochs', action='store_true',
-                    help='save epoch progress flag')
+
+def create_parser():
+    """Create CLI. Functionalized for code folding."""
+    parser = ArgumentParser('Lunar lander RL playground')
+    parser.add_argument(
+        'N', nargs='?', help='number of experiments to run', default=10, type=int)
+    parser.add_argument('--eps', default=0.25, type=float,
+                        help='random action probability (def:0.25)')
+    parser.add_argument('--alpha', default=0.5, type=float,
+                        help='learning rate (def:0.5)')
+    parser.add_argument('--gamma', default=0.5, type=float,
+                        help='discount parameter (def:0.5)')
+    parser.add_argument('--render-mode', default=None, help='gym render mode')
+    parser.add_argument('--load-fn', help='filename for load. Default: lander.txt',
+                        default='lander.txt')
+    parser.add_argument('--save-fn', help='filename for save. Default: lander.txt',
+                        default='lander.txt')
+    parser.add_argument('--load', action='store_true',
+                        help='load from fn flag')
+    parser.add_argument('--save', action='store_true', help='save to fn flag')
+    parser.add_argument('--overwrite', action='store_true',
+                        help='overwrite save file flag')
+    parser.add_argument('--load-params', action='store_true',
+                        help='load params from fn flag')
+    parser.add_argument('--plot', action='store_true', help='plot data flag')
+    parser.add_argument('--epoch-size', default=10000, type=int,
+                        help='number of experiments per epoch')
+    parser.add_argument('--save-epochs', action='store_true',
+                        help='save epoch progress flag')
+    return parser
+
 
 if __name__ == '__main__':
+    parser = create_parser()
     args = parser.parse_args()
 
     # Setup
